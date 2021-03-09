f5c9a60a218c343c30390e8d93061167a06ded8e
Tan Theng Lee <thengleetan@gmail.com>
2021-03-09 16:49:04 +0800

    Ex 1 quiz time solution: Read entire csv

--- 1-quiz-time-solutions/main.go
+++ 1-quiz-time-solutions/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -19,5 +20,11 @@ func main() {
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilenamePtr))
 	}
-	_ = file
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	fmt.Println(lines)
 }
