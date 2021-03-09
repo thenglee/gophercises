b28b79bfd515ca5713413bc5de71db13a4b21e3f
Tan Theng Lee <thengleetan@gmail.com>
2021-03-09 16:39:25 +0800

    Ex 1 quiz time solution: Refactor exit code to exit function

--- 1-quiz-time-solutions/main.go
+++ 1-quiz-time-solutions/main.go
@@ -6,14 +6,18 @@ import (
 	"os"
 )
 
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
 func main() {
 	csvFilenamePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilenamePtr)
 	if err != nil {
-		fmt.Printf("Failed to open the CSV file: %s\n", *csvFilenamePtr)
-		os.Exit(1)
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilenamePtr))
 	}
 	_ = file
 }
