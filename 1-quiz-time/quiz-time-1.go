ab3a04c64e126a48a2b23bede2f83305bdd13b67
Tan Theng Lee <thengleetan@gmail.com>
2021-03-09 14:39:30 +0800

    Ex 1 Quiz time: Add command line flag for filename

--- 1-quiz-time/quiz-time-1.go
+++ 1-quiz-time/quiz-time-1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -9,8 +10,12 @@ import (
 )
 
 func main() {
+	// Setup flag for filename
+	fileNamePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer' (default \"problems.csv\")")
+	flag.Parse()
+
 	// Open the file
-	csvfile, err := os.Open("problems.csv")
+	csvfile, err := os.Open(*fileNamePtr)
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
@@ -37,6 +42,7 @@ func main() {
 		// Prompt question
 		fmt.Printf("Question %d: %s = ", numQnsAsked, record[0])
 		fmt.Scan(&ans)
+		// Check for correct answer
 		if ans == record[1] {
 			numCorrectAns++
 		}
