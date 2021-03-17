ee3ab07e7b53b5cbc60fdc86497c00c96722899c
Tan Theng Lee <thengleetan@gmail.com>
2021-03-17 12:41:54 +0800

    Ex3 Cyoa solution: Decode json file data into Story type

--- 3-choose-yr-own-adventures-solution/cmd/cyoaweb/main.go
+++ 3-choose-yr-own-adventures-solution/cmd/cyoaweb/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+
+	"github.com/thenglee/3-choose-yr-own-adventures-solution/cyoa"
 )
 
 func main() {
 	filenamePtr := flag.String("file", "gopher.json", "the JSON file with CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *filenamePtr)
+
+	f, err := os.Open(*filenamePtr)
+	if err != nil {
+		panic(err)
+	}
+
+	d := json.NewDecoder(f)
+	var story cyoa.Story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", story)
 }
---
+++ 3-choose-yr-own-adventures-solution/go.mod
@@ -0,0 +1,3 @@
+module github.com/thenglee/3-choose-yr-own-adventures-solution
+
+go 1.13
