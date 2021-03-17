37091aed91120baa0d05570e2b545e157bba293b
Tan Theng Lee <thengleetan@gmail.com>
2021-03-17 16:57:23 +0800

    Ex3 Cyoa solution: Build http.Handler and render intro story

--- 3-choose-yr-own-adventures-solution/cmd/cyoaweb/main.go
+++ 3-choose-yr-own-adventures-solution/cmd/cyoaweb/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/thenglee/3-choose-yr-own-adventures-solution/cyoa"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	filenamePtr := flag.String("file", "gopher.json", "the JSON file with CYOA story")
+	portPtr := flag.Int("post", 3000, "the port to start the CYOA web application on")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *filenamePtr)
 
@@ -22,5 +25,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", story)
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *portPtr)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), h))
+
 }
--- 3-choose-yr-own-adventures-solution/cyoa/story.go
+++ 3-choose-yr-own-adventures-solution/cyoa/story.go
@@ -2,7 +2,9 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
 
 type Story map[string]Chapter
@@ -26,6 +28,27 @@ func JsonStory(r io.Reader) (Story, error) {
 	}
 	return story, nil
 }
+
+func NewHandler(s Story) http.Handler {
+	return &handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
 
 var defaultHandlerTmpl = `
 <!DOCTYPE html>
