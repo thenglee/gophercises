c334305b8afb9f8dfdc29b926381fb1eeb6402b7
Tan Theng Lee <thengleetan@gmail.com>
2021-03-17 12:57:05 +0800

    Ex3 Cyoa solution: Refactor decoding json data to story into cyoa package

--- 3-choose-yr-own-adventures-solution/cyoa/story.go
+++ 3-choose-yr-own-adventures-solution/cyoa/story.go
@@ -1,5 +1,10 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type Story map[string]Chapter
 
 type Chapter struct {
@@ -11,4 +16,13 @@ type Chapter struct {
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	var story Story
+	d := json.NewDecoder(r)
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
 }
