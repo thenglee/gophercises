7052198739d571b5fbef20262fce1bca7c99bfb2
Tan Theng Lee <thengleetan@gmail.com>
2021-03-18 11:17:13 +0800

    Ex3 Cyoa solution: NewHandler accepts custom html template

--- 3-choose-yr-own-adventures-solution/cyoa/story.go
+++ 3-choose-yr-own-adventures-solution/cyoa/story.go
@@ -31,12 +31,16 @@ func JsonStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
-func NewHandler(s Story) http.Handler {
-	return &handler{s}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+	}
+	return &handler{s, t}
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
