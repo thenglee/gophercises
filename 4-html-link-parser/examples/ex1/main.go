041f639c4891ede6c5462b63abb4ee6106085cb3
Tan Theng Lee <thengleetan@gmail.com>
2021-03-19 14:22:17 +0800

    Ex 4 HTML Link Parser: Open html file

--- 4-html-link-parser/examples/ex1/main.go
+++ 4-html-link-parser/examples/ex1/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 	// "github.com/thenglee/4-html-link-parser/parse"
 )
 
 func main() {
-	fmt.Println("ex 1")
+	filename := "ex1.html"
+	f, err := os.Open(filename)
+	_ = f
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("File opened successfully")
 }
