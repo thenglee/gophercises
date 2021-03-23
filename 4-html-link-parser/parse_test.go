359552ab76933a8d7aca16a22cf6ca1fbf70a479
Tan Theng Lee <thengleetan@gmail.com>
2021-03-23 15:00:17 +0800

    Ex 4 HTML Link Parser: Add more test scenarios for GetLinks

--- 4-html-link-parser/parse_test.go
+++ 4-html-link-parser/parse_test.go
@@ -21,3 +21,32 @@ func TestGetLinks(t *testing.T) {
 		}
 	}
 }
+
+func TestGetLinksNested(t *testing.T) {
+	s := `<p>Links:</p><ul><li><a href="https://www.twitter.com/joncalhoun">Check me out on twitter<i class="fa fa-twitter" aria-hidden="true"></i></a><li><a href="https://github.com/gophercises">Gophercises is on <strong>Github</strong>!</a></ul>`
+	links := parse.GetLinks(strings.NewReader(s))
+	expectedLinks := []parse.Link{
+		parse.Link{Href: "https://www.twitter.com/joncalhoun", Text: "Check me out on twitter"},
+		parse.Link{Href: "https://github.com/gophercises", Text: "Gophercises is on Github !"},
+	}
+
+	for i, link := range links {
+		if link != expectedLinks[i] {
+			t.Error(fmt.Sprintf("Expected %v but got %v.\n", expectedLinks[i], link))
+		}
+	}
+}
+
+func TestGetLinksWithComment(t *testing.T) {
+	s := `<html><body><a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a></body></html>`
+	links := parse.GetLinks(strings.NewReader(s))
+	expectedLinks := []parse.Link{
+		parse.Link{Href: "/dog-cat", Text: "dog cat"},
+	}
+
+	for i, link := range links {
+		if link != expectedLinks[i] {
+			t.Error(fmt.Sprintf("Expected %v but got %v.\n", expectedLinks[i], link))
+		}
+	}
+}
