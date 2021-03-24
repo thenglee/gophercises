ee0e1464372c8cf3be7c2424c5f37e24bc25e1db
Tan Theng Lee <thengleetan@gmail.com>
2021-03-24 16:44:08 +0800

    Ex5 Sitemap: Get url and parse html body in links struct

---
+++ 5-sitemap/go.mod
@@ -0,0 +1,5 @@
+module github.com/thenglee/5-sitemap
+
+go 1.13
+
+require golang.org/x/net v0.0.0-20210324051636-2c4c8ecb7826
--- 5-sitemap/main.go
+++ 5-sitemap/main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/thenglee/5-sitemap/link"
 )
 
 func main() {
 	urlPtr := flag.String("URL", "https://www.calhoun.io/", "URL to generate sitemap")
 	flag.Parse()
 
-	fmt.Println(*urlPtr)
+	resp, err := http.Get(*urlPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := strings.NewReader(string(body))
+	links, err := link.Parse(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", links)
 }
