6e80861b5993562e9af8964c78b84d4070513b43
Tan Theng Lee <thengleetan@gmail.com>
2021-03-25 15:26:53 +0800

    Ex5 Sitemap: Collect links from the same domain

--- 5-sitemap/main.go
+++ 5-sitemap/main.go
@@ -32,5 +32,44 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", links)
+	// fmt.Printf("%+v\n", links)
+
+	sameDomainLinks := linksInSameDomain(*urlPtr, links)
+	for _, l := range sameDomainLinks {
+		fmt.Println(l)
+	}
+}
+
+func linksInSameDomain(rootUrl string, links []link.Link) []string {
+	rootUrl = trimRootUrl(rootUrl)
+	var sameDomainLinks []string
+	rootUrlLen := len(rootUrl)
+
+	// Acceptable links: "rootUrl/path-with-domain", "/just-the-path"
+	for _, link := range links {
+		href := link.Href
+		hrefLen := len(href)
+		// Exclude "/" root path
+		if hrefLen > 1 {
+			if hrefLen > rootUrlLen {
+				// Starts with same domain
+				if link.Href[0:rootUrlLen] == rootUrl {
+					sameDomainLinks = append(sameDomainLinks, href)
+				}
+				// Starts with "/"
+			} else if href[0:1] == "/" {
+				sameDomainLinks = append(sameDomainLinks, rootUrl+href)
+			}
+		}
+	}
+	return sameDomainLinks
+}
+
+func trimRootUrl(rootUrl string) string {
+	// Remove trailing slash from rootUrl if present
+	length := len(rootUrl)
+	if rootUrl[length-1:length] == "/" {
+		rootUrl = rootUrl[:length-1]
+	}
+	return rootUrl
 }
