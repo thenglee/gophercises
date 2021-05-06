a4b5a5829d818e951576df8a93c298f98d46cc05
Tan Theng Lee <thengleetan@gmail.com>
2021-05-06 15:28:16 +0800

    Ex5 Sitemap solution: Parse and build links

---
+++ 5-sitemap-solution/go.mod
@@ -0,0 +1,5 @@
+module github.com/thenglee/5-sitemap-solution
+
+go 1.13
+
+require golang.org/x/net v0.0.0-20210505214959-0714010a04ed
--- 5-sitemap-solution/main.go
+++ 5-sitemap-solution/main.go
@@ -3,31 +3,64 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/thenglee/5-sitemap-solution/link"
 )
+
+/*
+	1. GET the webpage
+	2. parse all the links on the page
+	3. build proper urls with our links
+	4. filter out any links with a different domain
+	5. find all the pages (BFS)
+	6. print out xml
+*/
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
-
+	// Get webpage
 	resp, err := http.Get(*urlFlag)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	io.Copy(os.Stdout, resp.Body)
+	// Construct base url
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	// Parse links on page
+	links, _ := link.Parse(resp.Body)
+
+	// Build links
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, h := range hrefs {
+		fmt.Println(h)
+	}
 
 	/*
-		1. GET the webpage
-		2. parse all the links on the page
-		3. build proper urls with our links
-		4. filter out any links with a different domain
-		5. find all the pages (BFS)
-		6. print out xml
+		/some-path
+		https://gophercises/some-path
+		#fragment
+		mailto:jon@calhoun.io
 	*/
+
 }
